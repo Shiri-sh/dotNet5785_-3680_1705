db5bf796631f65d8187655a3cc91b454545076ca
shiri shahor <shirishahor@gmail.com>
2024-11-03 20:07:27 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program3680.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+namespace stage0 {
+    partial class program 
+    {
+        static void Main(string[] args)
+        {
+            welcom3680();
+            welcom1705();
+            Console.ReadKey();
+        }
+
+        private static void welcom3680()
+        {
+            Console.Write("enter your name: ");
+            string username = Console.ReadLine();
+            /*----------------------------------------------------------------------*/
+            Console.WriteLine(username + " welcom to my first console application");
+        }
+        static partial void welcom1705();
+     
+ 
+    }
+}
