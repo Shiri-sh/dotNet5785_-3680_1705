d6867aa5d8a1600175dd9e47df64054e90b52e12
ITammyDinavetsky <t0583235695@gmail.com>
2024-11-17 18:24:32 +0200

    stage 1 part: 4,5,6

--- DalFacade/DO/Assignmet.cs
+++ DalFacade/DO/Assignmet.cs
@@ -8,7 +8,6 @@
 /// <param name="Treatment_entry_time"></param>
 /// <param name="Treatment_end_time"></param>
 /// <param name="Type_of_treatment_termination"></param>
-
 public enum Type_of_treatment_termination {Handled, Self_cancellation, Concelling_administrator, Cancellation_Expired }
 public record Assignment
  (
---
+++ DalList/Config.cs
@@ -0,0 +1,21 @@
+﻿namespace Dal
+{
+    internal static class Config
+    {
+        //count call id auto
+        internal const int startCallId = 1;
+        private static int nextCallId = startCallId;
+        internal static int NextCallId { get=> nextCallId++; }
+
+        ///count auto
+        internal const int startAssignmentId = 1;
+        private static int nextAssignmentId = startAssignmentId;
+        internal static int NextAssignmentId { get => nextAssignmentId++; }
+
+        internal static void Reset()
+        {
+            nextCallId = 0; 
+            nextAssignmentId = 0;
+        }
+    }
+}
