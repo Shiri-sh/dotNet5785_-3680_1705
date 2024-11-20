4a5619d9e7bdb8e83e0b6fd9a8f8cddf8901cc33
shiri shahor <shirishahor@gmail.com>
2024-11-20 20:55:08 +0200

    תיעוד לממשקים

--- DalList/AssignmetImplementation.cs
+++ DalList/AssignmetImplementation.cs
@@ -7,37 +7,58 @@ using System.Collections.Generic;
 
 internal class AssignmetImplementation : IAssignment
 {
+    /// <summary>
+    /// Adding a new object of type Assigment to a database,
+    /// </summary>
+    /// <param name="item">exist Assigment object</param>
     public void Create(Assignment item)
     {
-        int id = DataSource.Config.NextCourseId;
+        int id = Config.NextAssignmentId;
         Assignment copy = item with { Id = id };
         DataSource.Assignments.Add(copy);
 
     }
-
+    /// <summary>
+    /// Deletion of an existing object with a certain ID, from a list of objects of type Assigment.
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <exception cref="NotImplementedException"></exception>
     public void Delete(int id)
     {
         if (Read(id) is null)
             throw new NotImplementedException("Assignment with such Id doesnt exist");
         DataSource.Assignments.RemoveAll(assignment => assignment.Id == id);
     }
-
+    /// <summary>
+    /// Deletion of all objects in a list of type Assignment
+    /// </summary>
     public void DeleteAll()
     {
         DataSource.Assignments.Clear();
 
     }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <returns>Returning a reference to a single object of type Assignment with a certain ID, if it exists in a database, or null if the object does not exist.</returns>
     public Assignment? Read(int id)
     {
         return DataSource.Assignments.Find(assignment => assignment.Id == id);
     }
-
+    /// <summary>
+    /// Return a copy of the list of references to all objects from a type
+    /// </summary>
+    /// <returns>Return a copy of the list of references to all objects from a type</returns>
     public List<Assignment> ReadAll()
     {
         return new List<Assignment>(DataSource.Assignments);
     }
-
+    /// <summary>
+    /// Update of an existing object.
+    /// </summary>
+    /// <param name="item">An existing object of type Assignment is updated.</param>
+    /// <exception cref="NotImplementedException">If there is no object with the received ID number - an exception will be thrown</exception>
     public void Update(Assignment item)
     {
         if (Read(item.Id) is null)
--- DalList/VolunteerImplementation.cs
+++ DalList/VolunteerImplementation.cs
@@ -5,13 +5,21 @@ using DalApi;
 using System.Collections.Generic;
 public class VolunteerImplementation : IVolunteer
 {
+    /// <summary>
+    /// Adding a new object of type Volunteer to a database,
+    /// </summary>
+    /// <param name="item">exist Volunteer object</param>
     public void Create(Volunteer item)
     {
         if (Read(item.Id) is not null)
             throw new NotImplementedException("the volunteer's id is already exist");
         DataSource.Volunteers.Add(item); 
     }
-
+    /// <summary>
+    /// Deletion of an existing object with a certain ID, from a list of objects of type Volunteer.
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <exception cref="NotImplementedException"></exception>
     public void Delete(int id)
     {
         if (Read(id) is null)
@@ -19,21 +27,35 @@ public class VolunteerImplementation : IVolunteer
         DataSource.Volunteers.RemoveAll(Volunteer=>Volunteer.Id==id);
 
     }
+    /// <summary>
+    /// Deletion of all objects in a list of type Volunteer
+    /// </summary>
     public void DeleteAll()
     {
         DataSource.Volunteers.Clear();
     }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <returns>Returning a reference to a single object of type Volunteer with a certain ID, if it exists in a database, or null if the object does not exist.</returns>
     public Volunteer? Read(int id)
     {
          return DataSource.Volunteers.Find(volunteer => volunteer.Id == id);
     }
-
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns>Return a copy of the list of references to all objects from a type</returns>
     public List<Volunteer> ReadAll()
     {
         return new List<Volunteer>(DataSource.Volunteers);
     }
-
+    /// <summary>
+    /// Update of an existing object.
+    /// </summary>
+    /// <param name="item">An existing object of type Volunteer is updated.</param>
+    /// <exception cref="NotImplementedException">If there is no object with the received ID number - an exception will be thrown</exception>
     public void Update(Volunteer item)
     {
         if (Read(item.Id) is null) 
