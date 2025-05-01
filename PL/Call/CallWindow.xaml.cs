3ca225903bdf13e89f77de354322615c981945c5
ITammyDinavetsky <t0583235695@gmail.com>
2025-05-01 22:53:53 +0300

    contunue stage 5

--- PL/Call/CallWindow.xaml.cs
+++ PL/Call/CallWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PL
+namespace PL.Call
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CallWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CallWindow : Window
     {
-        public MainWindow()
+        public CallWindow()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
