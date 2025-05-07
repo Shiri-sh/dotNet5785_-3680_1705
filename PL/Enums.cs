8098477da6d0b1e0e7cc354645e4bf669b05b37f
ITammyDinavetsky <t0583235695@gmail.com>
2025-05-07 18:21:02 +0300

    continue stage5

---
+++ PL/Enums.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL;
+
+internal class KindOfCallCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.KindOfCall> s_enums =
+(Enum.GetValues(typeof(BO.KindOfCall)) as IEnumerable<BO.KindOfCall>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+internal class PositionCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.Position> s_enums =
+(Enum.GetValues(typeof(BO.Position)) as IEnumerable<BO.Position>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+internal class StatusCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.Status> s_enums =
+(Enum.GetValues(typeof(BO.Status)) as IEnumerable<BO.Status>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+internal class StatusCallInProgressCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.StatusCallInProgress> s_enums =
+(Enum.GetValues(typeof(BO.StatusCallInProgress)) as IEnumerable<BO.StatusCallInProgress>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+internal class TypeOfDistanceCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.TypeOfDistance> s_enums =
+(Enum.GetValues(typeof(BO.TypeOfDistance)) as IEnumerable<BO.TypeOfDistance>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+internal class TypeOfTimeCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.TypeOfTime> s_enums =
+(Enum.GetValues(typeof(BO.TypeOfTime)) as IEnumerable<BO.TypeOfTime>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+internal class TypeOfTreatmentTerminationCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.TypeOfTreatmentTermination> s_enums =
+(Enum.GetValues(typeof(BO.TypeOfTreatmentTermination)) as IEnumerable<BO.TypeOfTreatmentTermination>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
