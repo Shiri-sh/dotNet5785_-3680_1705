497f5418092287c09729aa3afb389c474ac8cb3d
shiri shahor <shirishahor@gmail.com>
2025-06-09 18:23:49 +0300

    observer to call in progress

--- BL/BlImplementation/CallImplementation.cs
+++ BL/BlImplementation/CallImplementation.cs
@@ -131,6 +131,14 @@ internal class CallImplementation : ICall
         });
         CallManager.Observers.NotifyItemUpdated(assignId);
         CallManager.Observers.NotifyListUpdated();
+
+        IVolunteer vol = new VolunteerImplementation();
+        var volunteer = vol.Read(volunteerId);
+        if (volunteer.CallInProgress?.Id == assignId) // בדיקה כדי לאפס רק אם זו הקריאה הנוכחית
+        {
+            volunteer.CallInProgress = null;
+            vol.UpdateVolunteer(volunteer.Id,volunteer); //update the volunteer and the observer
+        }
     }
     public void UpdateEndCall(int volunteerId, int assignId)
     {
